5bae71d9a433ca14b6051ad153abe1a478fc501e
acmday <acmday@sina.com>
2020-07-30 22:06:42 +0800

    JavassistTest

---
+++ provider/src/test/java/com/acmday/dubbo/provider/jdk/JavassistTest.java
@@ -0,0 +1,39 @@
+package com.acmday.dubbo.provider.jdk;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.CtNewMethod;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author acmday.
+ * @date 2020/7/30.
+ */
+public class JavassistTest {
+
+    @Test
+    public void helloWorld() {
+        ClassPool classPool = ClassPool.getDefault();
+        CtClass ctClass = classPool.makeClass("HelloWorld");
+        try {
+            CtMethod ctMethod = CtNewMethod.make("public void test() {\n" +
+                    "        System.out.println(\"Hello World!\");\n" +
+                    "    }", ctClass);
+            ctClass.addMethod(ctMethod);
+            Class aClass = ctClass.toClass();
+
+            Object ob = aClass.newInstance();
+            Method method = aClass.getDeclaredMethod("test", null);
+            method.invoke(ob, null);
+
+        } catch (CannotCompileException | IllegalAccessException | InstantiationException
+                | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
